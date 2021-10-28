e62618452d1eeebb3dc69c85b1716a8b12a2b0b2
Evgeny Kurbatsky <kurbatsky@gmail.com>
2021-10-28 22:30:58 +0300

    Progressive rendering

--- templates/python/render_app.py
+++ templates/python/render_app.py
@@ -10,12 +10,14 @@ from PIL import Image, ImageTk
 class RenderApp:
     def __init__(self, draw_function, width, height, result_file=None):
         self.draw_function = draw_function
+        self.width = width
+        self.height = height
         self.root = tk.Tk()
 
-        self.array = np.zeros([400, 600, 3]).astype(np.uint8)
+        self.array = np.zeros([height, width, 3]).astype(np.uint8)
         self.img = ImageTk.PhotoImage(image=Image.fromarray(self.array))
 
-        self.canvas = tk.Canvas(self.root, width=600, height=400)
+        self.canvas = tk.Canvas(self.root, width=width, height=height)
         self.canvas.pack()
         self.c_image = self.canvas.create_image(0, 0, anchor="nw", image=self.img)
 
@@ -36,15 +38,21 @@ class RenderApp:
         self.root.after(100, self.do_update)
 
     def draw_all(self):
-        local_image = np.zeros((200, 300, 3))
-        while True:
-            for j in range(200):
-                for i in range(300):
-                    c = self.draw_function(i, j, 300, 200)
-                    local_image[j, i, :] = (c.clip(0, 0.9999) * 256)
+        width = self.width
+        height = self.height
+
+        local_image = np.zeros((height, width, 3))
+        mask = np.zeros((height, width))
+        for step in [16, 4, 1]:
+            for j in range(0, height, step):
+                for i in range(0, width, step):
+                    c = self.draw_function(i, j, width, height)
+                    if mask[j, i] != 1:
+                        local_image[j:j+step, i:i+step, :] = (c.clip(0, 0.9999) * 256)
+                        mask[j, i] = 1
 
                 with self.lock:
-                    self.array = resize(local_image, (400, 600), order=1).astype(np.uint8)
+                    self.array = local_image.astype(np.uint8)
 
 
 def run_render(draw_pixel, width, height, result_file):
