97712fa21ca9a3bbc8c4e4012882346a1ec83491
Evgeny Kurbatsky <kurbatsky@gmail.com>
2021-11-06 17:07:13 +0300

    Update render app

--- render_app.py
+++ render_app.py
@@ -1,7 +1,12 @@
+import time
 import tkinter as tk
+
+import tqdm
 
 import numpy as np
 import threading
+import matplotlib.pyplot as plt
+
 from skimage.transform import resize
 
 from PIL import Image, ImageTk
@@ -12,7 +17,10 @@ class RenderApp:
         self.draw_function = draw_function
         self.width = width
         self.height = height
+        self.result_file = result_file
         self.root = tk.Tk()
+
+        self.image_data = np.zeros((height, width, 3))
 
         self.array = np.zeros([height, width, 3]).astype(np.uint8)
         self.img = ImageTk.PhotoImage(image=Image.fromarray(self.array))
@@ -32,27 +40,43 @@ class RenderApp:
 
     def do_update(self):
         with self.lock:
-            self.img = ImageTk.PhotoImage(image=Image.fromarray(self.array))
-            self.c_image = self.canvas.create_image(0, 0, anchor="nw", image=self.img)
+            array = self.image_data.astype(np.uint8)
+        self.img = ImageTk.PhotoImage(image=Image.fromarray(array))
+        self.c_image = self.canvas.create_image(0, 0, anchor="nw", image=self.img)
 
         self.root.after(100, self.do_update)
 
+    def generate_pairs(self, step):
+        for j in range(0, self.height, step):
+            for i in range(0, self.width, step):
+                yield i, j
+
+    def generate_triples(self):
+        for step in [16, 4, 1]:
+            for i, j in self.generate_pairs(step):
+                yield step, i, j
+
     def draw_all(self):
+        start_time = time.time()
+
         width = self.width
         height = self.height
 
-        local_image = np.zeros((height, width, 3))
         mask = np.zeros((height, width))
-        for step in [16, 4, 1]:
-            for j in range(0, height, step):
-                for i in range(0, width, step):
-                    c = self.draw_function(i, j, width, height)
-                    if mask[j, i] != 1:
-                        local_image[j:j+step, i:i+step, :] = (c.clip(0, 0.9999) * 256)
-                        mask[j, i] = 1
+        triples = list(self.generate_triples())
+        for step, i, j in tqdm.tqdm(triples):
+            c = self.draw_function(i, j, width, height)
+            if mask[j, i] == 1:
+                continue
+            with self.lock:
+                self.image_data[j:j + step, i:i + step, :] = (c.clip(0, 0.9999) * 256)
+                mask[j, i] = 1
 
-                with self.lock:
-                    self.array = local_image.astype(np.uint8)
+        elapsed_time = time.time() - start_time
+        print('Rendering finished in {:.2f} s'.format(elapsed_time))
+
+        if self.result_file is not None:
+            plt.imsave('image.png', self.image_data.astype(np.uint8))
 
 
 def run_render(draw_pixel, width, height, result_file):
